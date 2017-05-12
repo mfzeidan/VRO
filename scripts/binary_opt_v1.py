2329dd8f8b2ba9fd1c9fe0d3846a4b48fc6fe59b
Mark Zeidan <mfzeidan@gmail.com>
2017-05-12 14:17:10 -0400

    Update binary_opt_v1.py

--- scripts/binary_opt_v1.py
+++ scripts/binary_opt_v1.py
@@ -8,6 +8,7 @@ import pandas as pd
 vball_model = pulp.LpProblem("marks first attempt",pulp.LpMaximize)
 
 
+###todo - future goal is to get entire rotation working
 
 idx = [1, 2,3,4,5,6]
 d = {'channel': pd.Series(['Player1', 'Player2', 'Player3','Player4','Player5','Player6'], index=idx),
@@ -19,13 +20,25 @@ d = {'channel': pd.Series(['Player1', 'Player2', 'Player3','Player4','Player5','
      'p2': pd.Series([0,0,0,0,0,0], index=idx)}
 full_rotation = pd.DataFrame(d)
 
+##################################################################################################
+
+#### HERE IS WHERE WE SET UP OUR VARIABLES TO ALLOW THE SOLVER TO DECIDE WHO'S IN AND OUT ########
+
+##################################################################################################
 
 
-FR_x1=LpVariable("FRP1",0,1,LpInteger)
+FR_x1=LpVariable("FRP1",0,1,LpInteger) ## 0 is the lowerbound, 1 is the upper bound. Only way I know to set up binary variables
 FR_x2=LpVariable("FRP2",0,1,LpInteger)
 
 BR_x1=LpVariable("BRP1",0,1,LpInteger)
 BR_x2=LpVariable("BRP2",0,1,LpInteger)
+
+##################################################################################################
+
+#### HERE IS WHERE WE BUILD THE ROTATION SKELETON ########
+
+#note that for now it is only a 2x2 matrix
+##################################################################################################
 
 
 idx = [1, 2]
@@ -33,17 +46,35 @@ d3 = {'A-Player': pd.Series(['Player1', 'Player2'], index=idx),
      'FR': pd.Series([FR_x1,FR_x2], index=idx),
      'BR': pd.Series([BR_x1,BR_x2], index=idx),
 }
+
 small_rotation = pd.DataFrame(d3)
 
+## above df looks like this
+#  A-Player    BR    FR
+#1  Player1  BRP1  FRP1
+#2  Player2  BRP2  FRP2
 
 
 
+
+#####################################################################################
+
+#### THIS IS THE PLAYER VALUE TABLE CREATION PORTION OF THE SCRIPT########
+
+#####################################################################################
 idx = [1, 2]
 d2 = {'A-Player': pd.Series(['Player1', 'Player2'], index=idx),
      'FR': pd.Series([2,3], index=idx),
      'BR': pd.Series([5,3], index=idx)}
 
 skill_levels = pd.DataFrame(d2)
+
+#######note this is what the above dataframe will look like
+### named the player column "a-Player" because pandas puts the columns in alphabetical order and i wanted it at front
+
+#  A-Player  BR  FR
+#1  Player1   5   2
+#2  Player2   3   3
 
 
 
@@ -124,8 +155,4 @@ for v in vball_model.variables():
     print(v.name, "=", v.varValue)
 print "---------------"
 print total_rotation_value
-
-
-
-
 
