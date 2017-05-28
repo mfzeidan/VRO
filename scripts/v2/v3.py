adf0535213bb95314a5c3f22556b8a89675065af
Mark Zeidan <mfzeidan@gmail.com>
2017-05-28 11:56:29 -0400

    >not pumping out code

--- scripts/v2/v3.py
+++ scripts/v2/v3.py
@@ -40,32 +40,32 @@ key_list = []
 
 
 p1_name =  "Mark1"
-p1_FR = 1
-p1_BR = 0
+p1_FR = 2
+p1_BR = 1
 p1_setter = 1
 
 p2_name = "Mark2"
-p2_FR = 1
-p2_BR = 0
+p2_FR = 2
+p2_BR = 1
 p2_setter = 0
 
 p3_name =  "Mark3"
-p3_FR = 1
-p3_BR = 0
+p3_FR = 3
+p3_BR = 1
 p3_setter = 1
 
 p4_name =  "Mark4"
-p4_FR = 0
+p4_FR = 2
 p4_BR = 1
 p4_setter = 0
 
 p5_name =  "Mark5"
-p5_FR = 0
+p5_FR = 2
 p5_BR = 1
 p5_setter = 0
 
 p6_name =  "Mark6"
-p6_FR = 0
+p6_FR = 2
 p6_BR = 1
 p6_setter = 0
 
@@ -105,7 +105,7 @@ small_rotation = pd.DataFrame(d3)
 
 
 idx = [1,2,3,4,5,6]
-d2 = {'A-Player': pd.Series(['Player1', 'Player2','Player3','Player4','Player5','Player6'], index=idx),
+d2 = {'A-Player': pd.Series(names, index=idx),
      'FR': pd.Series(FR_skills, index=idx),
      'BR': pd.Series(BR_skills, index=idx)}
 
@@ -192,6 +192,10 @@ for v in vball_model.variables():
 
 
 
+
+
+
+
 FR_br_skillset_dict = {}
 
 for x in starting_list_FR:
@@ -275,43 +279,214 @@ FR_FINAL = [p4,p3,two_spot]
 
 
 
+
 setters = {p1_name: p1_setter,p2_name:p2_setter,p3_name:p3_setter,p4_name:p4_setter,p5_name:p5_setter,p6_name:p6_setter}
 
 
+
+
+print "FR: 4:3:2"
+print "BR: 5:6:1"
+
+
+
+full_initial_rotation = [names[five_spot-1],names[p6-1],names[p1-1],names[p4-1],names[p3-1],names[two_spot-1]]
 
 
 
 rotation_setters_initial = {names[five_spot-1]:setters[names[five_spot-1]],names[p6-1]:setters[names[p6-1]],names[p1-1]:setters[names[p1-1]],names[p4-1]:setters[names[p4-1]],names[p3-1]:setters[names[p3-1]],names[two_spot-1]:setters[names[two_spot-1]]}
 
 
-print rotation_setters_initial
-
-print "<br>"
-print "<center><h1>"
-print "----------------"
-print "<br>"
-print names[five_spot-1]
-print setters[names[five_spot-1]]
-
-print names[p6-1]
-print setters[names[p6-1]]
-
-print names[p1-1]
-print setters[names[p1-1]]
-print "</h1></center>"
+### lets loop through the setter dict above and find out where each setter is starting
 
 
-print "<br>"
-print "<center><h1>"
-print names[p4-1]
-print setters[names[p4-1]]
 
-print names[p3-1]
-print setters[names[p3-1]]
 
-print names[two_spot-1]
-print setters[names[two_spot-1]]
+###### write a function that moves the setters around
 
-print "</h1></center>"
+
+def setter_construction(initial_rotation):
+
+	setter_counter = 0
+	
+	mini_set_dict = {}
+
+
+	### find out where both setters are
+	for key,value in initial_rotation.iteritems():
+		setter_counter += 1
+		#print(key,value)
+		if value == 1:
+			#### note that we are adding the values to the dictionary on a 1-6 counter, not the proper vball rotation counter
+			#### so if the setter dict is values 4 and 6, that actually means the setters are starting in the 5 and 1 spot
+
+			#print key
+			#print setter_counter
+
+			mini_set_dict[key] = setter_counter
+
+	### now the function needs to declare where both setters are
+
+	print mini_set_dict
+
+
+	BR_set_list = []
+	FR_set_list = []
+
+
+	for key,value in mini_set_dict.iteritems():
+		if value > 3:
+			BR_set_list.append(value)
+		else:
+			FR_set_list.append(value)
+
+	print BR_set_list
+	print len(BR_set_list)
+
+	print FR_set_list
+	print len(FR_set_list)	
+
+
+	
+
+	### lets figure out what name is what index in the pandas df
+	#print "finding range here"
+
+	name_list = {}
+	for x in range(1,7):
+		new_name = skill_levels.lookup([x],["A-Player"])[0]
+		name_list[new_name] = x
+
+
+	#print name_list
+
+
+	####if both setters are in BR
+
+	if len(BR_set_list) > 1:
+		print "both setters in BR"
+
+		#print initial_rotation
+
+		BR_setters_FR_skill = {}
+		for key,value in initial_rotation.iteritems():
+			if value == 1:
+				## key gives me the setters names
+				#print "player name"
+				#print key
+
+				#print "player number within the player list"
+				#print name_list[key]
+				#print name_list[key]
+
+
+				### now that we found the player name and their respective position in the skill levels dataframe, we can look up their FR skills
+
+				#print "this is each setters FR skill level"
+				BR_skill = skill_levels.lookup([name_list[key]],["FR"])[0]
+				BR_setters_FR_skill[key] = BR_skill
+
+		#### this will need to be fixed if both setters have equal skillsets
+
+		better_FR = max(BR_setters_FR_skill.iteritems(), key=operator.itemgetter(1))[0]
+		worse_FR_setter = min(BR_setters_FR_skill.iteritems(), key=operator.itemgetter(1))[0]
+
+
+		### this is the player of the 2 setters that is better in the FR
+		print "new function"
+		print BR_setters_FR_skill
+
+		for key, value in BR_setters_FR_skill.iteritems() :
+			print key, value
+
+		two_BR_setters(better_FR,worse_FR_setter)
+
+
+
+
+
+	#### if this is the case, lets do a lookup on each setters FR skillset
+
+
+	
+
+
+
+
+	if len(FR_set_list) > 1:
+		print "both setters in FR"
+
+
+def two_BR_setters(which_setter,sticky_setter):
+	print "this setter moves opposite of the stickey setter"
+	print which_setter
+	print rotation_setters_initial
+	print full_initial_rotation
+	print "this is the sticky setter"
+	print sticky_setter
+	### at this point we need to figure out where the setter that is staying in the backrow is in the rotation
+
+
+	### note the difference between the vball numbers and the index numbers
+		##FR: 4:3:2"
+		##BR: 5:6:1"
+
+
+	### index
+		##fr: 0,1,2
+		##br: 3,4,5
+
+	dont_move_setter = full_initial_rotation.index(sticky_setter)
+
+	print dont_move_setter
+
+	
+
+	### since we are dealing with 2 backrow setters, we can make the assumption that there are 3 possibilities for the rotation setup
+	## if the sticky setter is in 5 (index wise), then the new setter must move to 0
+	## else if 4, then 1
+	## else if 3, 2
+
+	if dont_move_setter == 5:
+		# if the sticky setter is in 5, lets swap the other setter with the player in the 0 spot in the list index
+	
+
+	
+
+setter_construction(rotation_setters_initial)
+
+
+
+
+
+#print rotation_setters_initial
+
+# print "<br>"
+# print "<center><h1>"
+# print "----------------"
+# print "<br>"
+# print names[five_spot-1]
+# print setters[names[five_spot-1]]
+
+# print names[p6-1]
+# print setters[names[p6-1]]
+
+# print names[p1-1]
+# print setters[names[p1-1]]
+# print "</h1></center>"
+
+
+# print "<br>"
+# print "<center><h1>"
+# print names[p4-1]
+# print setters[names[p4-1]]
+
+# print names[p3-1]
+# print setters[names[p3-1]]
+
+# print names[two_spot-1]
+# print setters[names[two_spot-1]]
+
+# print "</h1></center>"
 
 
